c511a5e22c6f140bcacbe2209bcb5b99be8b2989
Paola-Serra <paola.serra@sdggroup.com>
2023-01-31 10:18:18 +0100

    update metrics -mae

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,3 +1,15 @@
 function is_best(old_loss, new_loss)
     return old_loss > new_loss
 end
+
+
+
+#USE MEAN ABSOLUTE ERROR 
+#train
+error_train_PM  = Flux.Losses.mae(model(X) , test )
+error_test_RNN = Flux.Losses.mae(model_RNN(x_train) , y_train)
+#test
+error_test_PM  = Flux.Losses.mae( ŷ_PM , y )
+error_test_RNN = Flux.Losses.mae( ŷ_RNN , y )
+
+
