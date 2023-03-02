1709d9cc85a15d6d3dcba58aa33c37694e405b39
Paola-Serra <paola.serra@sdggroup.com>
2023-03-02 17:44:36 +0100

    METRICS GIF

--- src/gif.jl
+++ src/gif.jl
@@ -4,30 +4,7 @@ import Plots:Animation, buildanimation
 
 
 
-plot(epochs, loss_on_train_PM_adam, lab="PM", c=:green, alpha = 0.4, lw=2, ylims = (0,6));
-plot!(epochs, loss_on_train_adam, lab="CNN-1", c=:red, alpha = 0.4,lw=2, ylims = (0,6));
-plot!(epochs, loss_on_train_CNN_param_adj, lab="CNN-2", c=:blue, alpha = 0.4,lw=2, ylims = (0,6));
-#title!("Convolutional architecture with ADAM optimizer");
-yaxis!("Loss");
-xaxis!("Training epoch");
-savefig("loss_ADAM.pdf");
-
-losses = []
-push!(losses, loss_on_train_PM_adam)
-push!(losses, loss_on_train_adam)
-push!(losses,loss_on_train_CNN_param_adj)
-
-
-#plotting y predicted vs y true
-plot( y , alpha = 0.4,  lab= "y",lw=2)
-plot!( ŷ_PM ,alpha = 0.4, lab= "ŷ PM", lw=2) 
-plot!( ŷ_CNN, alpha = 0.4, lab= "ŷ CNN-1", lw=2)
-plot!( ŷ_CNN_2 , alpha = 0.4, lab= "ŷ CNN-2", lw=2)
-#title!("Predicted vs True");
-yaxis!("Energy demand");
-xaxis!("Time");
-savefig("energy_forecast.pdf");
-
+##trained user
 predictions=[]
 push!(predictions,y)
 push!(predictions,ŷ_PM)
@@ -36,7 +13,6 @@ push!(predictions,ŷ_CNN_2)
 
 labels = ["y","ŷ PM","ŷ CNN-1","ŷ CNN-2"]
 
-plot( predictions[1], lab = labels[1],c=:green ,alpha = 0.4, lw=2)
 
 j = 1
 for i in 1:4
@@ -59,3 +35,276 @@ nframes = 4
 fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
 anim = Animation("visualization/gif", fnames); 
 buildanimation(anim, "visualization/gif/original_data.gif", fps = 0.50, show_msg=false)
+
+
+
+
+###l-BFGS
+
+predictions_lbfgs=[]
+push!(predictions_lbfgs,y)
+push!(predictions_lbfgs,ŷ_PM_lbfgs)
+push!(predictions_lbfgs,ŷ_CNN_lbfgs)
+push!(predictions_lbfgs,ŷ_CNN2_lbfgs)
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_lbfgs[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_lbfgs[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/trained_lbfgs.gif", fps = 0.50, show_msg=false)
+
+
+#examples
+predictions_lbfgs=[]
+push!(predictions_lbfgs,y)
+push!(predictions_lbfgs,ŷ_PM_lbfgs)
+push!(predictions_lbfgs,ŷ_CNN_lbfgs)
+push!(predictions_lbfgs,ŷ_CNN2_lbfgs)
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_lbfgs[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_lbfgs[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/trained_lbfgs.gif", fps = 0.50, show_msg=false)
+
+
+#EXAMPLES
+predictions_adam_1143954=[]
+predictions_lbfgs_1143954=[]
+
+
+push!(predictions_adam_1143954, y_multi[3][:])
+push!(predictions_adam_1143954, ŷ_PM_multi[3])
+push!(predictions_adam_1143954, ŷ_CNN_multi[3])
+push!(predictions_adam_1143954, ŷ_CNN2_multi[3])
+
+push!(predictions_lbfgs_1143954, y_multi[3][:])
+push!(predictions_lbfgs_1143954, ŷ_PM_multi_lbfgs[3])
+push!(predictions_lbfgs_1143954, ŷ_CNN_multi_lbfgs[3])
+push!(predictions_lbfgs_1143954, ŷ_CNN2_multi_lbfgs[3])
+
+
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_adam_1143954[i],legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/adam/1143954/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_adam_1143954[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/adam/1143954/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/adam/1143954", fnames); 
+buildanimation(anim, "visualization/gif/adam/predictions_adam_1143954.gif", fps = 0.50, show_msg=false)
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_lbfgs_1143954[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/1143954/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_lbfgs_1143954[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/1143954/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/1143954/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/predictions_lbfgs_1143954.gif", fps = 0.50, show_msg=false)
+
+predictions_adam_1362155=[]
+predictions_lbfgs_1362155=[]
+
+
+push!(predictions_adam_1362155, y_multi[216][:])
+push!(predictions_adam_1362155, ŷ_PM_multi[216])
+push!(predictions_adam_1362155, ŷ_CNN_multi[216])
+push!(predictions_adam_1362155, ŷ_CNN2_multi[216])
+
+push!(predictions_lbfgs_1362155, y_multi[216][:])
+push!(predictions_lbfgs_1362155, ŷ_PM_multi_lbfgs[216])
+push!(predictions_lbfgs_1362155, ŷ_CNN_multi_lbfgs[216])
+push!(predictions_lbfgs_1362155, ŷ_CNN2_multi_lbfgs[216])
+
+
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_adam_1362155[i],legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/adam/1362155/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_adam_1362155[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/adam/1362155/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/adam/1362155", fnames); 
+buildanimation(anim, "visualization/gif/adam/predictions_adam_1362155.gif", fps = 0.50, show_msg=false)
+
+
+j = 1
+for i in 1:4
+    if i== 1
+        plot(predictions_lbfgs_1362155[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/1362155/" * @sprintf("%06d.png", j))
+        j +=1
+    else 
+        plot!(predictions_lbfgs_1362155[i], legend=false,alpha = 0.4, lw=2);
+        savefig("visualization/gif/lbfgs/1362155/" * @sprintf("%06d.png", j))
+        j +=1
+    end
+end
+
+nframes = 4         
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/1362155/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/predictions_lbfgs_1362155.gif", fps = 0.50, show_msg=false)
+
+
+
+### GENERALIZATION
+
+
+##Mse_adam
+
+j = 1
+for i in 1:3
+    if i== 1
+        Plots.histogram(MSE_test_PM, alpha = 0.4,label="PM", normalize=:pdf, color=:green)
+        savefig("visualization/gif/adam/MSE/" * @sprintf("%06d.png", j))
+        j +=1
+    elseif i==2
+        Plots.histogram!(MSE_test_CNN,alpha = 0.4, label="CNN-1", normalize=:pdf, color=:pink)
+
+        savefig("visualization/gif/adam/MSE/" * @sprintf("%06d.png", j))
+        j +=1
+    else
+        Plots.histogram!(MSE_test_CNN_2, alpha = 0.4,label="CNN-2", normalize=:pdf, color=:blue)
+        savefig("visualization/gif/adam/MSE/" * @sprintf("%06d.png", j))
+    end
+end
+
+nframes = 3  
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/adam/MSE/", fnames); 
+buildanimation(anim, "visualization/gif/adam/MSE/MSE_ADAM.gif", fps = 0.50, show_msg=false)
+
+
+############ MSE_lbfgs
+
+j = 1
+for i in 1:3
+    if i== 1
+        Plots.histogram(MSE_test_PM_lbfgs,legend=false, alpha = 0.4,bins=20, normalize=:pdf, color=:green)
+        savefig("visualization/gif/lbfgs/MSE/" * @sprintf("%06d.png", j))
+        j +=1
+    elseif i==2
+        Plots.histogram!(MSE_test_CNN_lbfgs, legend=false, alpha = 0.4,bins=:sqrt,normalize=:pdf, color=:pink)
+        savefig("visualization/gif/lbfgs/MSE/" * @sprintf("%06d.png", j))
+        j +=1
+    else
+        Plots.histogram!(MSE_test_CNN_2_lbfgs,legend=false, alpha = 0.4,  bins=20, normalize=:pdf, color=:blue)
+        savefig("visualization/gif/lbfgs/MSE/" * @sprintf("%06d.png", j))
+    end
+end
+
+nframes = 3  
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/MSE/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/MSE/MSE_lbfgs.gif", fps = 0.50, show_msg=false)
+
+
+
+#####MAE
+
+## ADAM
+
+
+j = 1
+for i in 1:3
+    if i== 1
+        Plots.histogram(MAE_test_PM,legend=false, alpha = 0.4,normalize=:pdf, color=:green)
+        savefig("visualization/gif/adam/MAE/" * @sprintf("%06d.png", j))
+        j +=1
+    elseif i==2
+        Plots.histogram!(MAE_test_CNN,legend=false, alpha = 0.4, normalize=:pdf, color=:pink)
+        savefig("visualization/gif/adam/MAE/" * @sprintf("%06d.png", j))
+        j +=1
+    else
+        Plots.histogram!(MAE_test_CNN_2, legend=false, alpha = 0.4, normalize=:pdf, color=:blue)
+        savefig("visualization/gif/adam/MAE/" * @sprintf("%06d.png", j))
+    end
+end
+
+nframes = 3  
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/adam/MAE/", fnames); 
+buildanimation(anim, "visualization/gif/adam/MAE/MAE_ADAM.gif", fps = 0.50, show_msg=false)
+
+
+
+###MAE LBFGS
+
+j = 1
+for i in 1:3
+    if i== 1
+        Plots.histogram(MAE_test_PM_lbfgs,legend=false, alpha = 0.4,bins=:sqrt, normalize=:pdf, color=:green)
+        savefig("visualization/gif/lbfgs/MAE/" * @sprintf("%06d.png", j))
+        j +=1
+    elseif i==2
+        Plots.histogram!(MAE_test_CNN_lbfgs, legend=false, alpha = 0.4, bins=:sqrt,normalize=:pdf, color=:pink)
+        savefig("visualization/gif/lbfgs/MAE/" * @sprintf("%06d.png", j))
+        j +=1
+    else
+        Plots.histogram!(MAE_test_CNN_2_lbfgs, legend=false, alpha = 0.4, bins=:sqrt, normalize=:pdf, color=:blue)
+        savefig("visualization/gif/lbfgs/MAE/" * @sprintf("%06d.png", j))
+    end
+end
+
+nframes = 3  
+fnames = [@sprintf("%06d.png", k) for k  in 1:nframes]   
+anim = Animation("visualization/gif/lbfgs/MAE/", fnames); 
+buildanimation(anim, "visualization/gif/lbfgs/MAE/MAE_lbfgs.gif", fps = 0.50, show_msg=false)
